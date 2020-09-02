afa1acea5396beb0570d75ba51b0abe16e4f392a
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-09-01 20:16:55 -0700

    5/10 tasks completed. MVP to come later.

--- js/index.js
+++ js/index.js
@@ -11,3 +11,38 @@ headerImage.addEventListener('wheel', function (event) {
 })
 
 // 2) keydown
+let headerNav = document.querySelector('.main-navigation')
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        headerNav.setAttribute('style',  'background-color:green')
+    }
+})
+
+// 3) mouseover
+let welcomeH2 = document.querySelector('.intro h2')
+
+welcomeH2.addEventListener('mouseover', function (event){
+    welcomeH2.style.fontSize = '75px'
+})
+
+// 4) load
+let paragraphTags = document.querySelectorAll('p')
+var x;
+
+window.addEventListener('load', function (event){
+    let temp = Array.from(paragraphTags)
+
+    for (x of temp){
+        x.style.transform = 'rotate(180deg)'
+    }
+})
+
+// 5) focus
+
+let footer = document.querySelector('.footer')
+
+footer.addEventListener('focus', function (event){
+    debugger
+    footer.setAttribute('style', 'background-color:green')
+}, true)
