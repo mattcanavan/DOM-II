e4e1e9df295fc5db5a6420486d8eed5f35bf7cd9
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-09-01 19:17:19 -0700

    Inital commit.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// 1) wheel
+let headerImage = document.querySelector('.home .intro img')
+
+headerImage.addEventListener('wheel', function (event) {
+    event.preventDefault();     //prevents webpage scroll
+    
+    let scale = 1;
+    scale += event.deltaY * -0.01;
+    headerImage.style.transform = `scale(${scale})`
+})
+
+// 2) keydown
